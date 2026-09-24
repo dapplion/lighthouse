f0fd2ff038376f03b236d3483aa9944ae88e4e42
dapplion <35266934+dapplion@users.noreply.github.com>
2026-09-24 13:07:49 +0200

    Use the payload block hash resolver on the re-org path

--- beacon_node/beacon_chain/src/beacon_chain.rs
+++ beacon_node/beacon_chain/src/beacon_chain.rs
@@ -5601,12 +5601,10 @@ impl<T: BeaconChainTypes> BeaconChain<T> {
             return Err(Box::new(DoNotReOrg::NotProposing.into()));
         }
 
-        // This only works pre-Gloas, but we don't run this code for Gloas anyway.
-        let parent_head_hash = info
-            .parent_node
-            .execution_status()
-            .ok()
-            .and_then(|execution_status| execution_status.block_hash());
+        let parent_head_hash = self
+            .canonical_head
+            .fork_choice_read_lock()
+            .payload_block_hash(&info.parent_node.root());
         let forkchoice_update_params = ForkchoiceUpdateParameters {
             head_root: info.parent_node.root(),
             head_hash: parent_head_hash,
